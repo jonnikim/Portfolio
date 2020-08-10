1fb1dfbac6aee467f5d2a3bcf493b934bfdd23f1
Jonathan <jonathan213kim@gmail.com>
2020-08-10 16:22:18 -0700

    new images

--- src/resumeData.js
+++ src/resumeData.js
@@ -127,14 +127,14 @@ let resumeData = {
     {
       name: "Note Taker",
       description: "Basic Note Taker using Nodejs",
-      imgurl: "images/portfolio/notetaker.png",
+      imgurl: "images/portfolio/notepad.png",
       url: "https://polar-caverns-98753.herokuapp.com/"
 
     },
     {
       name: "Model View Controller Application",
       description: "A simple 'eat a burger' application made to highlight MVC design paradigm",
-      imgurl: "images/portfolio/burgers.png",
+      imgurl: "images/portfolio/eatburger.png",
       url: "https://scenic-sequoia-26844.herokuapp.com/"
 
     },
